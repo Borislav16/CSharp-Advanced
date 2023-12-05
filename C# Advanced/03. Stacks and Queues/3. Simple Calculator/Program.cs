c429e784cd266b77a3f6c383683b9edfee68b3ec
Borislav16 <borko.g16@gmail.com>
2023-12-05 11:25:12 +0200

    exercise 3

---
+++ C# Advanced/03. Stacks and Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<string> expression = new Stack<string>(input.Reverse());
+
+            int result = int.Parse(expression.Pop());
+            while(expression.Count > 0)
+            {
+                string sign = expression.Pop();
+                int number = int.Parse(expression.Pop());
+                if (sign == "-")
+                {
+                    result -= number;
+                }
+                else if (sign == "+")
+                {
+                    result += number;
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
