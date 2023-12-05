82585eda5b8a2b620d040f6f7a215e0ff3ba4b82
Borislav16 <borko.g16@gmail.com>
2023-12-05 11:11:07 +0200

    exercise 2

---
+++ C# Advanced/03. Stacks and Queues/2. StackSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(input);
+            string command = Console.ReadLine().ToLower();
+            while(command != "end")
+            {
+                string[] commandInfo = command.Split(' ');
+                if (commandInfo[0] == "add")
+                {
+                    int n1 = int.Parse(commandInfo[1]);
+                    int n2 = int.Parse(commandInfo[2]);
+                    stack.Push(n1);
+                    stack.Push(n2);
+                }
+                else if (commandInfo[0] == "remove")
+                {
+                    int range = int.Parse(commandInfo[1]);
+                    while(range <= stack.Count && range > 0)
+                    {
+                        stack.Pop();
+                        range--;
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
