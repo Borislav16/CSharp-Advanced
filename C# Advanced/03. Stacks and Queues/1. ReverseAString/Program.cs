359a8c0a3fea8cab54f7457e0357cb7ccd5c9609
Borislav16 <borko.g16@gmail.com>
2023-12-05 10:51:34 +0200

    Stacks and Queues

---
+++ C# Advanced/03. Stacks and Queues/1. ReverseAString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> text = new Stack<char>(); 
+
+            foreach (var item in input)
+            {
+                text.Push(item);
+            }
+            Console.WriteLine(string.Join("",text));
+        }
+    }
+}
