a6d7f2641942ac72467b97d5a3f0397f681b24e1
Borislav16 <borko.g16@gmail.com>
2023-12-06 09:18:39 +0200

    exercise 5

---
+++ C# Advanced/03. Stacks and Queues/5. PrintEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbers = new Queue<int>(nums);
+            string result = string.Empty;
+            while (numbers.Count > 0)
+            {
+                int number = numbers.Dequeue();
+
+                if(number % 2 == 0)
+                {
+                    result += $"{number}, ";
+
+                }
+            }
+
+            Console.WriteLine(result.TrimEnd(' ', ','));
+        }
+    }
+}
