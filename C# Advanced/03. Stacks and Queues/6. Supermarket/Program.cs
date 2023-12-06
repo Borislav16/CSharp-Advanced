9fe01473e7db32f465ab677667592b109e0ba778
Borislav16 <borko.g16@gmail.com>
2023-12-06 10:08:27 +0200

    exercise 6

---
+++ C# Advanced/03. Stacks and Queues/6. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            string input = Console.ReadLine();
+            while(input != "End")
+            {
+                if(input != "Paid")
+                {
+                    names.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                while(names.Count > 0)
+                {
+                    Console.WriteLine(names.Dequeue());
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
