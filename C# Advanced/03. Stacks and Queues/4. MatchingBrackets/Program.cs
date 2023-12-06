fbbf20a0292391de2378eb56ff939956598a6ee0
Borislav16 <borko.g16@gmail.com>
2023-12-06 09:18:25 +0200

    exercise 4

---
+++ C# Advanced/03. Stacks and Queues/4. MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexes.Push(i);
+
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    string substring = input.Substring(startIndex, (i - startIndex) + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
