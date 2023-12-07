e86ae4bd33deb56e1f284d840e8f1a0fad31194e
Borislav16 <borko.g16@gmail.com>
2023-12-07 09:28:58 +0200

    exercise 8

---
+++ C# Advanced/03. Stacks and Queues/8. TrafficJam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            int count = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        string car = "";
+                        bool isTrue = queue.TryDequeue(out car);
+                        if (isTrue)
+                        {
+                            Console.WriteLine($"{car} passed!");
+                            count++;
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
