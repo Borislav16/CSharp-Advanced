9da66ebe89c67dccdfa55a7edc382c134c04ebe2
Borislav16 <borko.g16@gmail.com>
2023-12-08 10:52:31 +0200

    exercise 2

---
+++ C# Advanced/04. Exercise Stacks and Queues/02. BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>();
+            int elementsToEnqueue = commands[0];
+            int elementsToDequeue = commands[1];
+            int number = commands[2];
+
+            for (int i = 0; i < elementsToEnqueue; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(number))
+            {
+                Console.WriteLine("true");
+                return;
+            }
+
+            if (queue.Any())
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
