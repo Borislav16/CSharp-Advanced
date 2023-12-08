55af7188b2c54e0fa0f3d89fbeface60d0eed296
Borislav16 <borko.g16@gmail.com>
2023-12-08 11:17:58 +0200

    exercise 3

---
+++ C# Advanced/04. Exercise Stacks and Queues/03. MaximumandMinimumElement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._MaximumandMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (command[0] == "1")
+                {
+                    stack.Push(int.Parse(command[1]));
+                }
+                else if (command[0] == "2")
+                {
+                    if (stack.Any())
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (command[0] == "3")
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (command[0] == "4")
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
