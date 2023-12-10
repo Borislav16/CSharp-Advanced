50f28d284babc2ac8a641b77719c70e53ee76231
Borislav16 <borko.g16@gmail.com>
2023-12-10 11:42:13 +0200

    exercise 9

---
+++ C# Advanced/04. Exercise Stacks and Queues/09. SimpleTextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+
+            Stack<string> changes = new();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "1")
+                {
+                    changes.Push(text);
+                    text += command[1];
+                }
+                else if (command[0] == "2")
+                {
+                    changes.Push(text);
+                    int countOfErase = int.Parse(command[1]);
+                    text = text.Remove(text.Length - countOfErase);
+                }
+                else if (command[0] == "3")
+                {
+                    int index = int.Parse(command[1]) - 1;
+                    Console.WriteLine(text[index]);
+
+                }
+                else if (command[0] == "4")
+                {
+                    text = changes.Pop();
+                }
+            }
+        }
+    }
+}
