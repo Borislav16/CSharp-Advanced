04676375048776b50f98e8d9720adb114d044266
Borislav16 <borko.g16@gmail.com>
2023-12-10 11:42:01 +0200

    exercise 8

---
+++ C# Advanced/04. Exercise Stacks and Queues/08. BalancedParenthesis/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (char item in sequence)
+            {
+                switch (item)
+                {
+                    case '(':
+                    case '[':
+                    case '{': stack.Push(item);
+                        break;
+                    case ')':
+                        if(stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                }
+            }
+            if(stack.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
