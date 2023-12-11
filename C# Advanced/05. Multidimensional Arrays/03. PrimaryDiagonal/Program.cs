1ec6155c101ae60878b32d7c709f1ee640a189ee
Borislav16 <borko.g16@gmail.com>
2023-12-11 09:07:43 +0200

    exercise 3

---
+++ C# Advanced/05. Multidimensional Arrays/03. PrimaryDiagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _03._PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] array = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = array[j];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i,i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
