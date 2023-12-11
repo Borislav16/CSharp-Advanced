58e279141f35ec506835e07b3e13c757c7f8e516
Borislav16 <borko.g16@gmail.com>
2023-12-11 09:07:30 +0200

    exercise 2

---
+++ C# Advanced/05. Multidimensional Arrays/02. SumMatrixColumns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = arr[0];
+            int cols = arr[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] array = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = array[j];
+                }
+            }
+
+            
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sum += matrix[i, j];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+    
+}
