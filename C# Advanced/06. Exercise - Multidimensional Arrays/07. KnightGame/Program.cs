1824ef476e9597ecf54c49c9e98b10c8f5214bbc
Borislav16 <borko.g16@gmail.com>
2023-12-15 09:56:43 +0200

    Create Program.cs

---
+++ C# Advanced/06. Exercise - Multidimensional Arrays/07. KnightGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07._KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            if(size < 3)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+        }
+    }
+}
