fc77e5afa8511ffe1b3b2793feb39a048a5a6f1f
Borislav16 <borko.g16@gmail.com>
2023-12-16 11:30:45 +0200

    exercise 3

---
+++ C# Advanced/07. Sets and Dictionaries Advanced/03. Largest3Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            numbers = numbers.OrderByDescending(x => x).ToList();
+            if(numbers.Count < 3 )
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+                return;
+            
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
