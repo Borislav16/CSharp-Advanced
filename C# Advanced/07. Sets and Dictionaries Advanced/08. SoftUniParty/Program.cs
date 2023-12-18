fc71719fb9a4befb8f9cf2deeea39ecf45a2d6f0
Borislav16 <borko.g16@gmail.com>
2023-12-18 09:35:23 +0200

    Create Program.cs

---
+++ C# Advanced/07. Sets and Dictionaries Advanced/08. SoftUniParty/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            while(true)
+            {
+                string guest = Console.ReadLine();
+                
+                if (guest == "PARTY")
+                {
+                    while(guest != "END")
+                    {
+                        guest = Console.ReadLine();
+                        set.Remove(guest);
+                    }
+                    
+                    Console.WriteLine(set.Count);
+                    foreach (var item in set)
+                    {
+                        char[] ch = item.ToCharArray();
+                        if (char.IsDigit(ch[0]))
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    foreach (var item in set)
+                    {
+                        char[] ch = item.ToCharArray();
+                        if (char.IsLetter(ch[0]))
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    return;
+                }
+                set.Add(guest);
+            }
+        }
+    }
+}
