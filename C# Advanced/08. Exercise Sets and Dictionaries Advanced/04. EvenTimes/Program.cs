00022b2c26522d85f0fc20c481576f2f903afe5e
Borislav16 <borko.g16@gmail.com>
2023-12-20 10:04:17 +0200

    exercise 4

---
+++ C# Advanced/08. Exercise Sets and Dictionaries Advanced/04. EvenTimes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,int> numbers = new Dictionary<int,int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int givenNum = int.Parse(Console.ReadLine());
+                if(!numbers.ContainsKey(givenNum))
+                {
+                    numbers.Add(givenNum, 0);
+                }
+                numbers[givenNum]++;
+            }
+
+            foreach (var item in numbers)
+            {
+                if(item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
