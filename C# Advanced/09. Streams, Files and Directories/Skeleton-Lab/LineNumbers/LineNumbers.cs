7ac178f10d320189bd94f3b985b8ad09a8c3995e
Borislav16 <borko.g16@gmail.com>
2023-12-21 10:04:06 +0200

    exercise LineNumbers

---
+++ C# Advanced/09. Streams, Files and Directories/Skeleton-Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 0;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    Console.WriteLine(++counter + ". " + line);
+                }
+            }
+
+        }
+    }
+}
