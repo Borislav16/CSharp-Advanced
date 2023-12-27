d7dd0b992cc21d32b6b12444b37ea6bdf2a41655
Borislav16 <borko.g16@gmail.com>
2023-12-27 13:13:31 +0200

    Create Program.cs

---
+++ C# Advanced/11. Functional Programming/01. SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            Console.WriteLine(string.Join(", ",numbers.Where(x => x % 2 == 0)
+                .OrderBy(x => x)));
+        }
+    }
+}
