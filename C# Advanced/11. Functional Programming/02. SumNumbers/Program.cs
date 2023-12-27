a42382598fecde1a6c56a04c540209a49b17ff6b
Borislav16 <borko.g16@gmail.com>
2023-12-27 13:13:55 +0200

    Create Program.cs

---
+++ C# Advanced/11. Functional Programming/02. SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
