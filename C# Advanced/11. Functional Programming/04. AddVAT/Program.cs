0b6219bbce25868b3228c52f1f519082db83dbd1
Borislav16 <borko.g16@gmail.com>
2023-12-28 15:41:55 +0200

    Create Program.cs

---
+++ C# Advanced/11. Functional Programming/04. AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _04._AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] prices = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(n => n * 1.2m)
+                .ToArray();
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
