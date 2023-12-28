e9c64283c17a5ab91359e661d61b28873a77163f
Borislav16 <borko.g16@gmail.com>
2023-12-28 15:41:34 +0200

    Create Program.cs

---
+++ C# Advanced/11. Functional Programming/03. CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = w => char.IsUpper(w[0]);
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] upperCase = words.Where(func)
+                .ToArray();
+            Console.WriteLine(string.Join("\n", upperCase));
+                
+        }
+    }
+}
