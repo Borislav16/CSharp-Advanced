e3ae9c51ddd5e399a068f666535aa1d612308ab5
Borislav16 <borko.g16@gmail.com>
2023-12-30 17:45:53 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/01. ActionPrint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = n => Console.WriteLine(n);
+
+            string[] names = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            foreach (var name in names)
+            {
+                action(name);
+            }
+        }
+    }
+}
