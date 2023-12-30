1c92169f10f327ada475f33b902f42525d38ac5f
Borislav16 <borko.g16@gmail.com>
2023-12-30 17:47:12 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/02. KnightsOfHonor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = n => Console.WriteLine($"Sir {n}");
+
+            string[] names = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            foreach (var name in names)
+            {
+                action(name);
+            }
+        }
+    }
+}
