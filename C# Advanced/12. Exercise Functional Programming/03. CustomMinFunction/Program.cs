c9853dafd58fb86df3a4f69ee2c60045d1195c18
Borislav16 <borko.g16@gmail.com>
2023-12-30 17:47:44 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/03. CustomMinFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> func = n => n.Min();
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(func(numbers));
+        }
+    }
+}
