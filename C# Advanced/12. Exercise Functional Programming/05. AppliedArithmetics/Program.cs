ef0995ef3afa494a053726a9fff7f238f7ddf8f0
Borislav16 <borko.g16@gmail.com>
2023-12-31 12:32:33 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/05. AppliedArithmetics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Data;
+
+namespace _05._AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, List<int>, List<int>> calculate = (command, arr) =>
+            {
+                if (command == "add")
+                {
+                    for (int i = 0; i < arr.Count; i++)
+                    {
+                        arr[i] += 1;
+                    }
+                    return arr;
+
+                }
+                else if (command == "multiply")
+                {
+                    for (int i = 0; i < arr.Count; i++)
+                    {
+                        arr[i] *= 2;
+                    }
+                    return arr;
+                }
+                else if (command == "subtract")
+                {
+                    for (int i = 0; i < arr.Count; i++)
+                    {
+                        arr[i] -= 1;
+                    }
+                    return arr;
+                }
+                else if(command == "print")
+                {
+                     Console.WriteLine(string.Join(" ", arr));
+                     return arr; 
+                }
+                else
+                {
+                    return arr;
+                }
+            };
+            List<int> array = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                array = calculate(command, array);
+                command = Console.ReadLine();
+            }
+           
+        }
+    }
+}
