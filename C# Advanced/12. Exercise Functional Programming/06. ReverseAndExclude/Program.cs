aa6bee59f3a686b58d4b28a4510a02950ea3658d
Borislav16 <borko.g16@gmail.com>
2024-01-01 18:12:30 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/06. ReverseAndExclude/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06._ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> predicate = (num, divisor) =>
+            {
+                if (num % divisor != 0)
+                {
+                    return true;
+                }
+                return false;
+            };
+            List<int> list = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            int divisor = int.Parse(Console.ReadLine());
+
+            list = list.Where(x => predicate(x, divisor)).Reverse().ToList();
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
