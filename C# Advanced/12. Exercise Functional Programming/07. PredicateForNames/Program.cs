739c8732cdb8d871c305df6df10da91a6bd81bc3
Borislav16 <borko.g16@gmail.com>
2024-01-01 18:12:49 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/07. PredicateForNames/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07._PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, string, bool> names = (length, name) =>
+            {
+                if(length  >= name.Length)
+                {
+                    return true;
+                }
+                return false;
+            };
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            list = list.Where(name => names(n, name))
+                .ToList();
+            Console.WriteLine(string.Join("\n",list));
+        }
+    }
+}
