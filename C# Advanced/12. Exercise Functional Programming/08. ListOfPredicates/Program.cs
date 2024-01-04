2324e4d3e90397c14005438616dfa02cd2d5d214
Borislav16 <borko.g16@gmail.com>
2024-01-04 17:18:28 +0200

    Create Program.cs

---
+++ C# Advanced/12. Exercise Functional Programming/08. ListOfPredicates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _08._ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Predicate<int>> predicates = new();
+
+            int range = int.Parse(Console.ReadLine());
+
+            HashSet<int> dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToHashSet();
+
+            int[] numbers = Enumerable.Range(1, range).ToArray();
+
+            foreach (var divider in dividers)
+            {
+                
+                predicates.Add(n => n % divider == 0);
+            }
+
+            foreach (var number in numbers)
+            {
+                bool isDivisible = true;
+
+                foreach (var match in predicates)
+                {
+                    if (!match(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
