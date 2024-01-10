03908e181afcebdf7369bb58db52add2a2914316
Borislav16 <borko.g16@gmail.com>
2024-01-10 15:05:25 +0200

    Create Car.cs

---
+++ C# Advanced/14. Exercise Defining Classes/06. SpeedRacing/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption;
+        private double travelledDistance;
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumption = fuelConsumption;
+            this.TravelledDistance = 0;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public void Travel(double distance)
+        {
+            if(distance * fuelConsumption <= fuelAmount)
+            {
+                travelledDistance += distance;
+                FuelAmount -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        
+        }
+
+    }
+}
