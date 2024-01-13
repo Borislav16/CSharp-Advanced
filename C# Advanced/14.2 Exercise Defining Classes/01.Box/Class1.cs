edc048bb7aec6fb4b9e9d35ae8cbf52c6347b213
Borislav16 <borko.g16@gmail.com>
2024-01-13 09:38:00 +0200

    Create Class1.cs

---
+++ C# Advanced/14.2 Exercise Defining Classes/01.Box/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public T Remove()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot remove item: the list is empty");
+            }
+
+            T item = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return item;
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+    }
+}
