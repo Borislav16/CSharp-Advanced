b455a1f84f1e68248a619fa157aaf7516ca070c1
Borislav16 <borko.g16@gmail.com>
2024-01-14 11:04:54 +0200

    Create Program.cs

---
+++ C# Advanced/14.2 Exercise Defining Classes/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Test";
+            person.Age = 30;
+        }
+    }
+}
