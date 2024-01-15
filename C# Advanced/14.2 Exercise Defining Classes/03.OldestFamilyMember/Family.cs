6880754b55cc5db04da9996f57bfe4cca8bf3ed3
Borislav16 <borko.g16@gmail.com>
2024-01-15 16:29:24 +0200

    Create Family.cs

---
+++ C# Advanced/14.2 Exercise Defining Classes/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            people = new List<Person>();
+        }
+        public List<Person> people { get; set; }
+
+        public void AddMember(Person person)
+        {
+            people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = people.OrderByDescending(c => c.Age).FirstOrDefault();
+            return oldest;
+        }
+    }
+}
