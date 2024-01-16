ccadb5b9dfc23cb16b1a28b90e6d7b99aee8a57d
Borislav16 <borko.g16@gmail.com>
2024-01-16 14:25:13 +0200

    Create Program.cs

---
+++ C# Advanced/14.2 Exercise Defining Classes/04.OpinionPoll/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> peopleOver30 = new List<Person>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(input[0], int.Parse(input[1]));
+                if(person.Age > 30)
+                {
+                    peopleOver30.Add(person);
+                }
+            }
+            peopleOver30 = peopleOver30.OrderBy(n => n.Name).ToList();
+            foreach (var item in peopleOver30)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");   
+            }
+        }
+    }
+}
