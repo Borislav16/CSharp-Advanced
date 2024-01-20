9dc1d7090ee60e81cf50589da4de23ca60abdabf
Borislav16 <borko.g16@gmail.com>
2024-01-20 11:27:26 +0200

    Create Program.cs

---
+++ C# Advanced/17.Generics/01.Box/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Box<double> box = new Box<double>();
+
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            box.Add(4);
+
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
