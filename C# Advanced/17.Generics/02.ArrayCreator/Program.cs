caf64c10e3796dabf99ef581b868b2916dde51c9
Borislav16 <borko.g16@gmail.com>
2024-01-21 14:08:34 +0200

    Create Program.cs

---
+++ C# Advanced/17.Generics/02.ArrayCreator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericArrayCreator
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] list = ArrayCreator.Create(300, "dimitrichko");
+
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
