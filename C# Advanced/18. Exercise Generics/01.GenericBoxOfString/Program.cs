cc734e085750f717a34752e8385e9bee3e9a8311
Borislav16 <borko.g16@gmail.com>
2024-01-22 15:29:04 +0200

    Create Program.cs

---
+++ C# Advanced/18. Exercise Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.GenericBoxOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                Box<int> box = new(item);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
