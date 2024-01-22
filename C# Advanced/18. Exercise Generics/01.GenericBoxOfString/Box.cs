b4cb43ea82260633f0c7551b234cc7f68108efd6
Borislav16 <borko.g16@gmail.com>
2024-01-22 15:28:56 +0200

    Create Box.cs

---
+++ C# Advanced/18. Exercise Generics/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.GenericBoxOfString
+{
+    internal class Box<T>
+    {
+        private T generic;
+
+        public Box(T generic)
+        {
+            this.generic = generic;
+        }
+        
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {generic}";
+        }
+    }
+}
