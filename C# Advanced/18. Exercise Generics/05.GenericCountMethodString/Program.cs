fd7c77a592159893857f41ea009a194b21a1dee1
Borislav16 <borko.g16@gmail.com>
2024-01-23 15:01:40 +0200

    Create Program.cs

---
+++ C# Advanced/18. Exercise Generics/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.GenericCountMethodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                box.Add(number);
+            }
+
+            double compare = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Count(compare));
+        }
+    }
+}
