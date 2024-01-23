028ad9e86bc1b888d51f0980a3f0d7af7c27d982
Borislav16 <borko.g16@gmail.com>
2024-01-23 15:01:31 +0200

    Create Box.cs

---
+++ C# Advanced/18. Exercise Generics/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.GenericCountMethodString
+{
+    internal class Box<T> where T : IComparable<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public int Count(T compare)
+        {
+            int count = 0;
+            foreach (var item in list)
+            {
+                if (item.CompareTo(compare) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
