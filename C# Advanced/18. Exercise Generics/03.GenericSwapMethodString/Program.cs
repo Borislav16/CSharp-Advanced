821b7a3945a4d6ed80f727e3bc8e0f4a51d4f400
Borislav16 <borko.g16@gmail.com>
2024-01-23 15:00:39 +0200

    Create Program.cs

---
+++ C# Advanced/18. Exercise Generics/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.GenericSwapMethodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> items = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+
+                items.Add(item);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(indexes[0], indexes[1], items);
+
+            foreach (int item in items)
+            {
+                Console.WriteLine($"{typeof(int)}: {item}");
+            }
+
+        }
+        static void Swap<T>(int firstIndex, int secondIndex, List<T> items)
+        {
+            (items[secondIndex], items[firstIndex]) = (items[firstIndex], items[secondIndex]);
+        }
+    }
+}
