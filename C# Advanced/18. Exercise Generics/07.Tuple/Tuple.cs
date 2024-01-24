c86f6352d31fccccdd31ea14e655e3c49df4d228
Borislav16 <borko.g16@gmail.com>
2024-01-24 19:29:04 +0200

    Create Tuple.cs

---
+++ C# Advanced/18. Exercise Generics/07.Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Tuple
+{
+    internal class Tuple<T1, T2>
+    {
+        public Tuple(T1 item1, T2 item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T1 item1 { get; set; }
+        public T2 item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2}";
+        }
+    }
+}
