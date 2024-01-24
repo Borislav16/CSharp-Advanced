ef77406b2eb48d3b885b16c97fa57e05046f549b
Borislav16 <borko.g16@gmail.com>
2024-01-24 19:28:56 +0200

    Create Program.cs

---
+++ C# Advanced/18. Exercise Generics/07.Tuple/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndAddress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] nameAndBeer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] intAndDouble = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Tuple<string, string> tuple1 = new($"{nameAndAddress[0]} {nameAndAddress[1]}", nameAndAddress[2]);
+
+            Tuple<string, int> tuple2 = new(nameAndBeer[0], int.Parse(nameAndBeer[1]));
+
+            Tuple<int, double> tuple3 = new(int.Parse(intAndDouble[0]), double.Parse(intAndDouble[1]));
+
+            Console.WriteLine(tuple1.ToString());
+            Console.WriteLine(tuple2.ToString());
+            Console.WriteLine(tuple3.ToString());
+        }
+    }
+}
