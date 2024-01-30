e4deae6a446ab09ff34f052112803b5f698d05c9
Borislav16 <borko.g16@gmail.com>
2024-01-30 15:03:21 +0200

    Create Program.cs

---
+++ C# Advanced/23. Algorithms Introduction/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(RecursiveSum(list, 0));
+        }
+
+        public static int RecursiveSum(List<int> list, int index)
+        {
+            if(list.Count - 1 == index)
+            {
+                return list[index];
+            }
+
+            int sumOfRestElements = RecursiveSum(list, index + 1);
+
+            int sumofAllElements = list[index] + sumOfRestElements;
+
+            return sumofAllElements;
+        }
+    }
+}
