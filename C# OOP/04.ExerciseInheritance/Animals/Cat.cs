f2508a174e1a22a6364f50f2e58e60032d495b18
Borislav16 <borko.g16@gmail.com>
2024-02-02 14:12:20 +0200

    Create Cat.cs

---
+++ C# OOP/04.ExerciseInheritance/Animals/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+        public override string ProduceSound()
+        {
+            return "Meow meow";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Cat");
+            stringBuilder.AppendLine($"{Name} {Age} {Gender}");
+            stringBuilder.Append(ProduceSound());
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
