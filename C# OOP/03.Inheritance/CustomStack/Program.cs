e53acdaf45c3bb0c33376f652293a2b9457a1e5b
Borislav16 <borko.g16@gmail.com>
2024-02-02 14:11:31 +0200

    Create Program.cs

---
+++ C# OOP/03.Inheritance/CustomStack/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings strings = new StackOfStrings();
+            Console.WriteLine(strings.IsEmpty());
+            strings.AddRange(new string[] { "1", "2", "324", "64" });
+        }
+    }
+}
