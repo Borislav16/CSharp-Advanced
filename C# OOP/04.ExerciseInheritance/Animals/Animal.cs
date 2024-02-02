932000a2e5faf0737b47d4001632aa10adc98237
Borislav16 <borko.g16@gmail.com>
2024-02-02 14:11:47 +0200

    Create Animal.cs

---
+++ C# OOP/04.ExerciseInheritance/Animals/Animal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+		private int age;
+        private string gender;
+        public Animal(string name,int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name ;
+            }
+            private set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                name = value ;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                gender = value;
+            }
+        }
+
+        public int Age
+		{
+			get 
+            {
+                return age; 
+            }
+			set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                } 
+                age = value;
+            }
+		}
+        public virtual string ProduceSound()
+        {
+            return "";
+        }
+        
+    }
+}
