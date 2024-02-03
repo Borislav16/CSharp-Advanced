ad83fc336e7918c32228da3944768e5617a1b5ca
Borislav16 <borko.g16@gmail.com>
2024-02-03 11:47:51 +0200

    Create Frog.cs

---
+++ C# OOP/04.ExerciseInheritance/Animals/Frog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Frog : Animal
+    {
+        public Frog(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+        public override string ProduceSound()
+        {
+            return "Ribbit";
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Frog");
+            stringBuilder.AppendLine($"{Name} {Age} {Gender}");
+            stringBuilder.Append(ProduceSound());
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
