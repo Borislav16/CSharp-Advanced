593ba309263669031957048ffa728112091271bf
Borislav16 <borko.g16@gmail.com>
2024-02-03 11:47:40 +0200

    Create Dog.cs

---
+++ C# OOP/04.ExerciseInheritance/Animals/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Dog");
+            stringBuilder.AppendLine($"{Name} {Age} {Gender}");
+            stringBuilder.Append(ProduceSound());
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
