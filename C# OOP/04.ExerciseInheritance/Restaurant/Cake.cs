b862d11bc8ef6a0d15c4e672301bd4948af4b981
Borislav16 <borko.g16@gmail.com>
2024-02-07 15:40:46 +0200

    Create Cake.cs

---
+++ C# OOP/04.ExerciseInheritance/Restaurant/Cake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, CakePrice, Grams, Calories)
+        {
+        }
+
+        public const double Grams = 250;
+        public const double Calories = 1000;
+        public const int CakePrice = 5;
+        
+    }
+}
