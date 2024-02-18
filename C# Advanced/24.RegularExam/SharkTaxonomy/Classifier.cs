fe6d5845939e7ef24f2dc7b3bcb858d39e0e0f0e
Borislav16 <borko.g16@gmail.com>
2024-02-18 11:58:16 +0200

    Create Classifier.cs

---
+++ C# Advanced/24.RegularExam/SharkTaxonomy/Classifier.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public Classifier(int capacity)
+        {
+                Capacity = capacity;
+                Species = new List<Shark>();
+        }
+        public int Capacity { get; set; }
+
+        public List<Shark> Species { get; set; }
+
+        public int GetCount => Species.Count;
+
+        public void AddShark(Shark shark)
+        {
+            if (Species.Count < Capacity)
+            {
+                Shark givenShark = Species.FirstOrDefault(s => s.Kind == shark.Kind);
+                if (givenShark == null)
+                {
+                    Species.Add(shark);
+                }
+            }
+        }
+
+        public bool RemoveShark(string kind)
+        {
+            Shark givenShark = Species.FirstOrDefault(s => s.Kind == kind);
+            if (givenShark == null)
+            {
+                return false;
+            }
+            else
+            {
+                Species.Remove(givenShark);
+                return true;
+            }
+        }
+
+        public string GetLargestShark()
+        {
+            Shark shark = Species.OrderByDescending(s => s.Length).First();
+            return shark.ToString();
+        }
+
+        public double GetAverageLength()
+        {
+            double average = Species.Average(s => s.Length);
+            return average;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Species.Count} sharks classified:");
+            foreach (var shark in Species)
+            {
+                sb.AppendLine(shark.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
