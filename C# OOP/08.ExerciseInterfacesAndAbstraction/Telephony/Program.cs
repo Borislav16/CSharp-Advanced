d9e10af856ef30feb01e927d06384c21cdf3bc0f
Borislav16 <borko.g16@gmail.com>
2024-02-26 21:03:10 +0200

    Create Program.cs

---
+++ C# OOP/08.ExerciseInterfacesAndAbstraction/Telephony/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Telephony
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phones = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] urls = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ICallable callable;
+            foreach (var phone in phones)
+            {
+                if(phone.Length == 10)
+                {
+                    callable = new Smartphone();
+                }
+                else
+                {
+                    callable = new Stationaryphone();   
+                }
+
+                try
+                {
+                    Console.WriteLine(callable.Call(phone));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            IBrowsable browsable;
+            foreach (var url in urls)
+            {
+                browsable = new Smartphone();
+                try
+                {
+                    Console.WriteLine(browsable.Browse(url));
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
