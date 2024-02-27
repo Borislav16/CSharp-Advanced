84ec87f9f4c8b2828e76dae0dbfdab1a269c95cc
Borislav16 <borko.g16@gmail.com>
2024-02-27 10:12:48 +0200

    Create Stationaryphone.cs

---
+++ C# OOP/08.ExerciseInterfacesAndAbstraction/Telephony/Stationaryphone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephony
+{
+    public class Stationaryphone : ICallable
+    {
+        public string Call(string phoneNumber)
+        {
+            if (IsDigit(phoneNumber))
+            {
+
+                return $"Dialing... {phoneNumber}";
+
+            }
+            else
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+
+        }
+        private bool IsDigit(string phoneNumber)
+        {
+            return phoneNumber.All(x => char.IsDigit(x));
+        }
+    }
+}
