56edf0156a1b87786fd1fe48b75c8832523916f2
Borislav16 <borko.g16@gmail.com>
2024-02-29 08:47:52 +0200

    Create Shape.cs

---
+++ C# OOP/09.Polymorphism/Shapes/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Shape draw");
+        }
+    }
+}
