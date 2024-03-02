5a0fead00eb3170ba4719bfd0f3ea56bfd022aa3
Borislav16 <borko.g16@gmail.com>
2024-03-02 13:34:07 +0200

    Create Car.cs

---
+++ C# OOP/10.ExercisePolymorphism/CorrectVehicles2/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectVehicles2
+{
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double consumptionPerKm, int tankCapacity)
+           : base(fuelQuantity, consumptionPerKm, tankCapacity)
+        {
+
+        }
+    }
+}
