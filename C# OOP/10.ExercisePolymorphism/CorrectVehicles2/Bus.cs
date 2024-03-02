c84d5c8ef0cd930d6406a60bc00456952818d653
Borislav16 <borko.g16@gmail.com>
2024-03-02 13:34:00 +0200

    Create Bus.cs

---
+++ C# OOP/10.ExercisePolymorphism/CorrectVehicles2/Bus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectVehicles2
+{
+    public class Bus : Vehicle
+    {
+        public Bus(double fuelQuantity, double consumptionPerKm, int tankCapacity)
+            : base(fuelQuantity, consumptionPerKm, tankCapacity)
+        {
+        }
+    }
+}
