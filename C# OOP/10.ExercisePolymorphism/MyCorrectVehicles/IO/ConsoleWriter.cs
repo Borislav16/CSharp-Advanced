28bf55786b92661a0b9dc15b5acedb69dda8dac0
Borislav16 <borko.g16@gmail.com>
2024-03-03 09:51:27 +0200

    Create ConsoleWriter.cs

---
+++ C# OOP/10.ExercisePolymorphism/MyCorrectVehicles/IO/ConsoleWriter.cs
@@ -0,0 +1,14 @@
+﻿using MyCorrectVehicles.IO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCorrectVehicles.IO
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string str) => Console.WriteLine(str);
+    }
+}
