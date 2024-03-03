07408330732f19aafb465360aa3b9495031f3d91
Borislav16 <borko.g16@gmail.com>
2024-03-03 09:51:20 +0200

    Create ConsoleReader.cs

---
+++ C# OOP/10.ExercisePolymorphism/MyCorrectVehicles/IO/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿using MyCorrectVehicles.IO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCorrectVehicles.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine() => Console.ReadLine();
+        
+    }
+}
