3dfa110189865fb50ebf4e2cf3283a5889088214
Borislav16 <borko.g16@gmail.com>
2024-03-03 09:51:35 +0200

    Create FileWriter.cs

---
+++ C# OOP/10.ExercisePolymorphism/MyCorrectVehicles/IO/Interfaces/FileWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCorrectVehicles.IO.Interfaces
+{
+    public class FileWriter : IWriter
+    {
+        public void WriteLine(string str)
+        {
+            using StreamWriter sw = new StreamWriter("D:\\test.txt", true);
+
+            sw.WriteLine(str);
+        }
+        
+    }
+}
