eefd8937d62a17eb93f76525cdf9daf2782a9112
Borislav16 <borko.g16@gmail.com>
2024-03-05 08:41:05 +0200

    Create Factory.cs

---
+++ C# OOP/10.ExercisePolymorphism/Raiding/Factories/Factory.cs
@@ -0,0 +1,30 @@
+﻿using Raiding.Model.Interfaces;
+using Raiding.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Factories
+{
+    public class Factory : IFactory
+    {
+        public IHero Create(string type, string name)
+        {
+            switch (type)
+            {
+                case "Druid":
+                    return new Druid(name);
+                case "Paladin":
+                    return new Paladin(name);
+                case "Rogue":
+                    return new Rogue(name);
+                case "Warrior":
+                    return new Warrior(name);
+                default:
+                    throw new ArgumentException("Invalid hero!");
+            }
+        }
+    }
+}
