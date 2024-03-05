578a41dc4bf50e53ccdd807c0a24ba59b5bbd503
Borislav16 <borko.g16@gmail.com>
2024-03-05 08:40:34 +0200

    Create Program.cs

---
+++ C# OOP/10.ExercisePolymorphism/MyCorrectVehicles/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyCorrectVehicles.IO.Interfaces;
+using MyCorrectVehicles.IO;
+using MyCorrectVehicles.Core;
+
+namespace MyCorrectVehicles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            IEngine engine = new Engine(reader, writer);
+
+            engine.Run();
+        }
+    }
+}
