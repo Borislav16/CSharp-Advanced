bb909a34664718414a0f1ac1ed8b80ec495d55ec
Borislav16 <borko.g16@gmail.com>
2024-03-05 08:41:16 +0200

    Create IFactory.cs

---
+++ C# OOP/10.ExercisePolymorphism/Raiding/Factories/IFactory.cs
@@ -0,0 +1,14 @@
+﻿using Raiding.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Factories
+{
+    public interface IFactory
+    {
+        IHero Create(string type, string name);
+    }
+}
