e5df800817b49b17f729bdd803ffbec6ea826511
Borislav16 <borko.g16@gmail.com>
2024-03-06 09:41:36 +0200

    Create Hero.cs

---
+++ C# OOP/10.ExercisePolymorphism/Raiding/Model/Hero.cs
@@ -0,0 +1,24 @@
+﻿using Raiding.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Model
+{
+    public abstract class Hero : IHero
+    {
+        protected Hero(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+
+        public string Name { get; private set; }
+
+        public int Power { get; private set; }
+
+        public abstract string CastAbility();
+    }
+}
