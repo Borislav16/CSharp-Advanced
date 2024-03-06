5ba7407136c3b5bf890ce6177697bca6a43b8e7e
Borislav16 <borko.g16@gmail.com>
2024-03-06 09:41:26 +0200

    Create Druid.cs

---
+++ C# OOP/10.ExercisePolymorphism/Raiding/Model/Druid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Model
+{
+    public class Druid : Hero
+    {
+        private const int DefaultPower = 80;
+
+        public Druid(string name) : base(name, DefaultPower) { }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {Name} healed for {Power}";
+    }
+}
