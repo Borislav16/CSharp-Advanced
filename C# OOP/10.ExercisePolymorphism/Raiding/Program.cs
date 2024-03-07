5e87380d836a3569864b6b4be51f21fbc224c296
Borislav16 <borko.g16@gmail.com>
2024-03-07 10:54:01 +0200

    Create Program.cs

---
+++ C# OOP/10.ExercisePolymorphism/Raiding/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raiding.Core;
+using Raiding.Factories;
+using Raiding.IO;
+using Raiding.IO.Interfaces;
+using Raiding.Factories;
+
+namespace Raiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+            IFactory heroFactory = new Factory();
+
+            IEngine engine = new Engine(reader, writer, heroFactory);
+
+            engine.Run();
+        }
+    }
+}
