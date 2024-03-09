94be66ac72339589588ea4a3a8129c640c5dbfbc
Borislav16 <borko.g16@gmail.com>
2024-03-09 11:56:13 +0200

    Create IVehicleFactory.cs

---
+++ C# OOP/10.ExercisePolymorphism/VehiclesExtension/Factories/Interfaces/IVehicleFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.Model.Interfaces;
+
+namespace VehiclesExtension.Factories;
+
+public interface IVehicleFactory
+{
+    public IVehicle Create(string type, double fuelQuantity, double fuelConsumption, double tankCapacity);
+}
