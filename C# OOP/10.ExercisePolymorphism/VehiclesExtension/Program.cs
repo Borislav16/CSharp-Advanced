2dcb065dc9e922464e14962b6607cd18071a62c5
Borislav16 <borko.g16@gmail.com>
2024-03-10 15:18:50 +0200

    Create Program.cs

---
+++ C# OOP/10.ExercisePolymorphism/VehiclesExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using VehiclesExtension.Core;
+using VehiclesExtension.Factories;
+using VehiclesExtension.IO;
+using VehiclesExtension.IO.Interfaces;
+
+namespace VehiclesExtension
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+            IVehicleFactory vehicleFactory = new VehicleFactory();
+
+            IEngine engine = new Engine(reader, writer, vehicleFactory);
+
+            engine.Run();
+        }
+    }
+}
