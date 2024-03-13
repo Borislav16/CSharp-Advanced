f4ff941174af13a386ae89061c5cb0e66d8fe605
Borislav16 <borko.g16@gmail.com>
2024-03-13 08:15:31 +0200

    Create Dog.cs

---
+++ C# OOP/10.ExercisePolymorphism/WildFarm/Model/Animal/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Model.Foods;
+
+namespace WildFarm.Model.Animal
+{
+    public class Dog : Mammal
+    {
+        private const double DogMultiplier = 0.40;
+        public Dog(string name, double weight, string livingRegion) : base(name, weight, livingRegion)
+        {
+        }
+
+        public override IReadOnlyCollection<Type> PreferredFoods
+            => new HashSet<Type> { typeof(Meat)};
+
+        protected override double WeightMultiplier
+            => DogMultiplier;
+
+        public override string ProduceSound()
+            => $"Woof!";
+
+        public override string ToString()
+        => base.ToString() + $"{Weight}, {LivingRegion}, {FoodEaten}]";
+    }
+}
