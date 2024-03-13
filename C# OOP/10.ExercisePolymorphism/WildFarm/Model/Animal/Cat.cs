299c209b0f70a3da26f85c12490cfc62390bd965
Borislav16 <borko.g16@gmail.com>
2024-03-13 08:15:20 +0200

    Create Cat.cs

---
+++ C# OOP/10.ExercisePolymorphism/WildFarm/Model/Animal/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Model.Foods;
+
+namespace WildFarm.Model.Animal
+{
+    public class Cat : Feline
+    {
+        private const double CatMultiplier = 0.30;
+        public Cat(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override IReadOnlyCollection<Type> PreferredFoods
+            => new HashSet<Type> { typeof(Vegetable), typeof(Meat)};
+
+        protected override double WeightMultiplier => CatMultiplier;
+
+        public override string ProduceSound()
+            => $"Meow";
+    }
+}
