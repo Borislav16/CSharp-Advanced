285a4c976c24a2b0c3df033f7440d10161cf5ac2
Borislav16 <borko.g16@gmail.com>
2024-03-14 09:27:27 +0200

    Create Mouse.cs

---
+++ C# OOP/10.ExercisePolymorphism/WildFarm/Model/Animal/Mouse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Model.Foods;
+
+namespace WildFarm.Model.Animal
+{
+    public class Mouse : Mammal
+    {
+        private const double MouseMultiplier = 0.10; 
+        public Mouse(string name, double weight, string livingRegion) : base(name, weight, livingRegion)
+        {
+        }
+
+        public override IReadOnlyCollection<Type> PreferredFoods 
+            => new HashSet<Type> { typeof(Vegetable), typeof(Fruit)};
+
+        protected override double WeightMultiplier => MouseMultiplier;
+
+        public override string ProduceSound()
+            => $"Squeak";
+
+        public override string ToString()
+        => base.ToString() + $"{Weight}, {LivingRegion}, {FoodEaten}]";
+    }
+}
