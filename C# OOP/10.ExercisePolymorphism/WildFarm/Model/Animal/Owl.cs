2be80fe7c53c237ca21bd2799435efc4197b9b58
Borislav16 <borko.g16@gmail.com>
2024-03-14 09:27:41 +0200

    Create Owl.cs

---
+++ C# OOP/10.ExercisePolymorphism/WildFarm/Model/Animal/Owl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Model.Foods;
+
+namespace WildFarm.Model.Animal
+{
+    public class Owl : Bird
+    {
+        private const double OwlMultiplier = 0.25;
+        public Owl(string name, double weight, double wingSize) : base(name, weight, wingSize)
+        {
+        }
+
+        public override IReadOnlyCollection<Type> PreferredFoods
+            => new HashSet<Type> { typeof(Meat)};
+
+        protected override double WeightMultiplier
+            => OwlMultiplier;
+
+        public override string ProduceSound()
+        => $"Hoot Hoot";
+    }
+}
