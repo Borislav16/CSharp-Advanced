be3e90f82fd57e596ad90526ff08a7aabed92e18
Borislav16 <borko.g16@gmail.com>
2024-03-14 09:27:54 +0200

    Create Tiger.cs

---
+++ C# OOP/10.ExercisePolymorphism/WildFarm/Model/Animal/Tiger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Model.Foods;
+
+namespace WildFarm.Model.Animal
+{
+    public class Tiger : Feline
+    {
+        private const double TigerMultiplier = 1.00;
+        public Tiger(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override IReadOnlyCollection<Type> PreferredFoods
+            => new HashSet<Type> {typeof(Meat) };
+
+        protected override double WeightMultiplier => TigerMultiplier;
+
+        public override string ProduceSound()
+            => $"ROAR!!!";
+    }
+}
