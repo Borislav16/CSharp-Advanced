7237e12b245e7238348892eb0b3a18880be77728
Borislav16 <borko.g16@gmail.com>
2024-03-16 21:42:02 +0200

    Create Program.cs

---
+++ C# OOP/11.ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                    throw new ArgumentException("Invalid number.");
+
+                Console.WriteLine(Math.Sqrt(input));
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
