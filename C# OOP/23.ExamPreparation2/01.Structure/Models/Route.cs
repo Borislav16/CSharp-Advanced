75b97b12b566716bc2a4d82afc51e2b850019beb
Borislav16 <borko.g16@gmail.com>
2024-04-05 10:45:17 +0300

    Create Route.cs

---
+++ C# OOP/23.ExamPreparation2/01.Structure/Models/Route.cs
@@ -0,0 +1,88 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models
+{
+    public class Route : IRoute
+    {
+        public Route(string startPoint, string endPoint, double length, int routeId)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Length = length;
+            RouteId = routeId;
+        }
+
+        private string startPoint;
+
+        public string StartPoint
+        {
+            get { return startPoint; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.StartPointNull);
+                }
+                startPoint = value; 
+            }
+        }
+
+        private string endPoint;
+
+        public string EndPoint
+        {
+            get { return endPoint; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.EndPointNull);
+                }
+                endPoint = value;
+            }
+        }
+
+        private double length;
+
+        public double Length
+        {
+            get { return length; }
+            private set 
+            {
+                if(value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.RouteLengthLessThanOne);
+                }
+                length = value; 
+            }
+        }
+
+        private int routeId;
+
+        public int RouteId
+        {
+            get { return routeId; }
+            private set { routeId = value; }
+        }
+
+        private bool isLocked;
+
+        public bool IsLocked
+        {
+            get { return isLocked; }
+            private set { isLocked = value; }
+        }
+
+
+        public void LockRoute()
+        {
+            isLocked = true;
+        }
+    }
+}
