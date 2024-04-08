ec87ab42de5789cea8f642dfe89cfcbeb1677228
Borislav16 <borko.g16@gmail.com>
2024-04-08 23:24:32 +0300

    Create Goalkeeper.cs

---
+++ C# OOP/23.ExamPreparation3/01.Structure/Handball/Models/Goalkeeper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class Goalkeeper : Player
+    {
+        public Goalkeeper(string name) : base(name, 2.5)
+        {
+        }
+
+        public override void DecreaseRating()
+        {
+            if (Rating - 1.25 < 1)
+            {
+                Rating = 1;
+            }
+            else
+            {
+                Rating -= 1.25;
+            }
+        }
+
+        public override void IncreaseRating()
+        {
+            if(Rating + 0.75 > 10)
+            {
+                Rating = 10;
+            }
+            else
+            {
+                Rating += 0.75;
+            }
+        }
+    }
+}
