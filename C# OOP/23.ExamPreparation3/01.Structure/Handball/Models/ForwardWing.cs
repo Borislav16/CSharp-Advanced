7a3c43fedc7eb1654faaea4e7b1413b631e5b9ce
Borislav16 <borko.g16@gmail.com>
2024-04-08 23:24:25 +0300

    Create ForwardWing.cs

---
+++ C# OOP/23.ExamPreparation3/01.Structure/Handball/Models/ForwardWing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class ForwardWing : Player
+    {
+        public ForwardWing(string name) : base(name, 5.5)
+        {
+        }
+
+        public override void DecreaseRating()
+        {
+            if (Rating - 0.75 < 1)
+            {
+                Rating = 1;
+            }
+            else
+            {
+                Rating -= 0.75;
+            }
+        }
+
+        public override void IncreaseRating()
+        {
+            if (Rating + 1.25 > 10)
+            {
+                Rating = 10;
+            }
+            else
+            {
+                Rating += 1.25;
+            }
+        }
+    }
+}
