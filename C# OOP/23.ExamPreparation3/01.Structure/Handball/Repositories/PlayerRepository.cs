19a13bde8276ee2f8fa48eb815426b1f26867f71
Borislav16 <borko.g16@gmail.com>
2024-04-09 12:20:15 +0300

    Create PlayerRepository.cs

---
+++ C# OOP/23.ExamPreparation3/01.Structure/Handball/Repositories/PlayerRepository.cs
@@ -0,0 +1,51 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+
+        public PlayerRepository()
+        {
+            models = new List<IPlayer>();
+        }
+        private List<IPlayer> models;
+
+        public IReadOnlyCollection<IPlayer> Models
+        {
+            get { return models.AsReadOnly(); }
+            
+        }
+
+        public void AddModel(IPlayer model)
+        {
+            models.Add(model);
+        }
+
+        public bool ExistsModel(string name)
+        {
+            var model = models.FirstOrDefault(m => m.Name == name);
+            if(model != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public IPlayer GetModel(string name)
+        {
+            return models.FirstOrDefault(m => m.Name == name);
+        }
+
+        public bool RemoveModel(string name)
+        {
+            return models.Remove(models.FirstOrDefault(m => m.Name == name));
+        }
+    }
+}
