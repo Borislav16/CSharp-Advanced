a5e30f826eabc978398f6a4e78e91235b978db65
Borislav16 <borko.g16@gmail.com>
2024-04-10 10:31:04 +0300

    Create FreeDiver.cs

---
+++ C# OOP/23.ExamPreparation4/01.Structure/Models/FreeDiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Models
+{
+    public class FreeDiver : Diver
+    {
+        public FreeDiver(string name) : base(name, 120)
+        {
+        }
+
+        public override void Miss(int TimeToCatch)
+        {
+            double percentage = TimeToCatch * 0.60;
+            OxygenLevel -= (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
+            if (OxygenLevel <= 0)
+            {
+                if (!HasHealthIssues)
+                {
+                    UpdateHealthStatus();
+                }
+            }
+        }
+
+        public override void RenewOxy()
+        {
+            OxygenLevel = 120;
+        }
+    }
+}
