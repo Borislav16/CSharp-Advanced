60ce78fe18642577922411a39a5fa0fcfefc236b
Borislav16 <borko.g16@gmail.com>
2024-04-11 10:05:54 +0300

    Create ScubaDiver.cs

---
+++ C# OOP/23.ExamPreparation4/01.Structure/Models/ScubaDiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Models
+{
+    public class ScubaDiver : Diver
+    {
+        public ScubaDiver(string name) : base(name, 540)
+        {
+        }
+
+        public override void Miss(int TimeToCatch)
+        {
+            double percentage = TimeToCatch * 0.30;
+            OxygenLevel -= (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
+            if(OxygenLevel <= 0)
+            {
+                if(!HasHealthIssues)
+                {
+                    UpdateHealthStatus();
+                }
+            }
+        }
+
+        public override void RenewOxy()
+        {
+            OxygenLevel = 540;
+        }
+    }
+}
