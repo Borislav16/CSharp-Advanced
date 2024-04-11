f12c12be1d7339934ca8aed18ef853fea4320ab8
Borislav16 <borko.g16@gmail.com>
2024-04-11 10:06:40 +0300

    Create FishRepository.cs

---
+++ C# OOP/23.ExamPreparation4/01.Structure/Repositories/FishRepository.cs
@@ -0,0 +1,36 @@
+﻿using NauticalCatchChallenge.Models.Contracts;
+using NauticalCatchChallenge.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Repositories
+{
+    public class FishRepository : IRepository<IFish>
+    {
+        private List<IFish> models;
+
+        public FishRepository()
+        {
+            models = new List<IFish>();
+        }
+        public IReadOnlyCollection<IFish> Models
+        {
+            get { return models.AsReadOnly(); }
+
+        }
+
+
+        public void AddModel(IFish fish)
+        {
+            models.Add(fish);
+        }
+
+        public IFish GetModel(string name)
+        {
+            return models.FirstOrDefault(m => m.Name == name);
+        }
+    }
+}
