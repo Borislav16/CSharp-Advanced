49d0402fa3b073defb52a322af4f08a0752e7ee8
Borislav16 <borko.g16@gmail.com>
2024-04-13 18:02:40 +0300

    Create BookingRepository.cs

---
+++ C# OOP/24.ExamPreparation/01.Structure/Repositories/BookingRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private readonly List<IBooking> bookings;
+        
+        public BookingRepository()
+        {
+            bookings = new List<IBooking>();
+        }
+
+        public void AddNew(IBooking model)
+        {
+            bookings.Add(model);
+        }
+
+        public IBooking Select(string criteria)
+        {
+            return bookings.FirstOrDefault(b => b.BookingNumber.ToString() == criteria);
+        }
+
+        public IReadOnlyCollection<IBooking> All()
+        {
+            return bookings.AsReadOnly();
+        }
+
+    }
+}
