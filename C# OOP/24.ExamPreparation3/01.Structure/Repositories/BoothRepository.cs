9a46b5d6c27301c12c96c9e4b542ab6c9d122374
Borislav16 <borko.g16@gmail.com>
2024-04-18 11:31:45 +0300

    Create BoothRepository.cs

---
+++ C# OOP/24.ExamPreparation3/01.Structure/Repositories/BoothRepository.cs
@@ -0,0 +1,30 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class BoothRepository : IRepository<IBooth>
+    {
+        public BoothRepository()
+        {
+            models = new List<IBooth>();
+        }
+        private List<IBooth> models;
+
+        public IReadOnlyCollection<IBooth> Models
+        {
+            get { return models.AsReadOnly(); }
+
+        }
+
+
+        public void AddModel(IBooth model)
+        {
+            models.Add(model);
+        }
+    }
+}
