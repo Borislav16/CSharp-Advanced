cce8e16e2308d6434056820d02cc7f9438bb2612
Borislav16 <borko.g16@gmail.com>
2024-04-18 11:30:31 +0300

    Create Cocktail.cs

---
+++ C# OOP/24.ExamPreparation3/01.Structure/Models/Cocktails/Cocktail.cs
@@ -0,0 +1,72 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public abstract class Cocktail : ICocktail
+    {
+        protected Cocktail(string cocktailName, string size, double price)
+        {
+            Name = cocktailName;
+            Size = size;
+            Price = price;
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NameNullOrWhitespace);
+                }
+                name = value;
+            }
+        }
+
+        private string size;
+
+        public string Size
+        {
+            get { return size; }
+            private set
+            {
+                size = value;
+            }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            private set
+            {
+                if (Size == "Large")
+                {
+                    price = value;
+                }
+                else if (Size == "Middle")
+                {
+                    price = value * 2 / 3;
+                }
+                else if(Size == "Small")
+                {
+                    price = value * 1 / 3;
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Size}) - {this.Price:F2} lv";
+        }
+
+    }
+}
