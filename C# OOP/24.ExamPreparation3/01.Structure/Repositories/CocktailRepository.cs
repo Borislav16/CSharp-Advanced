4e0da83ce42eab949117bc820147a0fd34e643f5
Borislav16 <borko.g16@gmail.com>
2024-04-18 11:31:54 +0300

    Create CocktailRepository.cs

---
+++ C# OOP/24.ExamPreparation3/01.Structure/Repositories/CocktailRepository.cs
@@ -0,0 +1,29 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class CocktailRepository : IRepository<ICocktail>
+    {
+        public CocktailRepository()
+        {
+            models = new List<ICocktail>();
+        }
+        private List<ICocktail> models;
+
+        public IReadOnlyCollection<ICocktail> Models
+        {
+            get { return models.AsReadOnly(); }
+            
+        }
+       
+
+        public void AddModel(ICocktail model)
+        {
+            models.Add(model);
+        }
+    }
+}
