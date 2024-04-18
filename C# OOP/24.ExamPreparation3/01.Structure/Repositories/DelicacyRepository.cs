c869966e822c6d17ddad411e63eec103d87a4236
Borislav16 <borko.g16@gmail.com>
2024-04-18 11:32:59 +0300

    Create DelicacyRepository.cs

---
+++ C# OOP/24.ExamPreparation3/01.Structure/Repositories/DelicacyRepository.cs
@@ -0,0 +1,29 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class DelicacyRepository : IRepository<IDelicacy>
+    {
+        public DelicacyRepository()
+        {
+            models = new List<IDelicacy>();
+        }
+        private List<IDelicacy> models;
+
+        public IReadOnlyCollection<IDelicacy> Models
+        {
+            get { return models.AsReadOnly(); }
+            
+        }
+
+        public void AddModel(IDelicacy model)
+        {
+            models.Add(model);
+        }
+    }
+}
