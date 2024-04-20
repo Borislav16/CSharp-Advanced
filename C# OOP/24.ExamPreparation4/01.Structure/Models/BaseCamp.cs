b8316ca33af325124716dbf6d1550e1a227bf5bd
Borislav16 <borko.g16@gmail.com>
2024-04-20 09:54:17 +0300

    Create BaseCamp.cs

---
+++ C# OOP/24.ExamPreparation4/01.Structure/Models/BaseCamp.cs
@@ -0,0 +1,29 @@
+﻿using HighwayToPeak.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Models
+{
+    public class BaseCamp : IBaseCamp
+    {
+        public BaseCamp()
+        {
+            residents = new List<string>();
+        }
+        private List<string> residents;
+        public IReadOnlyCollection<string> Residents => residents.AsReadOnly();
+
+        public void ArriveAtCamp(string climberName)
+        {
+            residents.Add(climberName);
+        }
+
+        public void LeaveCamp(string climberName)
+        {
+            residents.Remove(climberName);
+        }
+    }
+}
