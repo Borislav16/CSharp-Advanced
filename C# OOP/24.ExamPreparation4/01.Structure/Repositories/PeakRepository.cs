df6ebbb8bbef71cc3141540ee574cdfa12ccab7f
Borislav16 <borko.g16@gmail.com>
2024-04-21 11:04:42 +0300

    Create PeakRepository.cs

---
+++ C# OOP/24.ExamPreparation4/01.Structure/Repositories/PeakRepository.cs
@@ -0,0 +1,30 @@
+﻿using HighwayToPeak.Models.Contracts;
+using HighwayToPeak.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Repositories
+{
+    public class PeakRepository : IRepository<IPeak>
+    {
+        public PeakRepository()
+        {
+            all = new List<IPeak>();
+        }
+        private List<IPeak> all;
+        public IReadOnlyCollection<IPeak> All => all.AsReadOnly();
+
+        public void Add(IPeak model)
+        {
+            all.Add(model);
+        }
+
+        public IPeak Get(string name)
+        {
+            return all.FirstOrDefault(a => a.Name == name);
+        }
+    }
+}
