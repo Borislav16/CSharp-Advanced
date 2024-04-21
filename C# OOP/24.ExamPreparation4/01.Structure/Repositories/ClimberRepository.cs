daca59b5378c548e24cf9ad7e1c2f452728be3d3
Borislav16 <borko.g16@gmail.com>
2024-04-21 11:04:25 +0300

    Create ClimberRepository.cs

---
+++ C# OOP/24.ExamPreparation4/01.Structure/Repositories/ClimberRepository.cs
@@ -0,0 +1,30 @@
+﻿using HighwayToPeak.Models.Contracts;
+using HighwayToPeak.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Repositories
+{
+    public class ClimberRepository : IRepository<IClimber>
+    {
+        public ClimberRepository()
+        {
+            all = new List<IClimber>();
+        }
+        private List<IClimber> all;
+        public IReadOnlyCollection<IClimber> All => all.AsReadOnly();
+
+        public void Add(IClimber model)
+        {
+            all.Add(model);
+        }
+
+        public IClimber Get(string name)
+        {
+            return all.FirstOrDefault(a => a.Name == name);
+        }
+    }
+}
