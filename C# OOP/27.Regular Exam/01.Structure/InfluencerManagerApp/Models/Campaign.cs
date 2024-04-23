1c646dbe793290f033e42ef41abc854317546010
Borislav16 <borko.g16@gmail.com>
2024-04-23 09:50:18 +0300

    Create Campaign.cs

---
+++ C# OOP/27.Regular Exam/01.Structure/InfluencerManagerApp/Models/Campaign.cs
@@ -0,0 +1,68 @@
+﻿using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfluencerManagerApp.Models
+{
+    public abstract class Campaign : ICampaign
+    {
+        protected Campaign(string brand, double budget)
+        {
+            Brand = brand;
+            Budget = budget;
+            contributors = new List<string>();
+        }
+        private string brand;
+
+        public string Brand
+        {
+            get { return brand; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.BrandIsrequired);
+                }
+                brand = value;
+            }
+        }
+
+        private double budget;
+
+        public double Budget
+        {
+            get { return budget; }
+            private set { budget = value; }
+        }
+
+
+        private List<string> contributors;
+
+        public IReadOnlyCollection<string> Contributors
+        {
+            get { return contributors.AsReadOnly(); }
+        }
+
+
+        public void Engage(IInfluencer influencer)
+        {
+            contributors.Add(influencer.Username);
+            influencer.EarnFee(influencer.CalculateCampaignPrice());
+            Budget -= influencer.CalculateCampaignPrice();
+        }
+
+        public void Gain(double amount)
+        {
+            Budget += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} - Brand: {Brand}, Budget: {Budget}, Contributors: {contributors.Count}";
+        }
+    }
+}
